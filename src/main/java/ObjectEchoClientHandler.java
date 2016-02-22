32ac17888a1f542aa93aaf3d79239f1d27edc19c
minudika <minudika@wso2.com>
2016-02-22 17:04:46 +0530

    Feeding received event objects to execution plan  Please enter the commit message for your changes. Lines starting

--- src/main/java/ObjectEchoClientHandler.java
+++ src/main/java/ObjectEchoClientHandler.java
@@ -1,8 +1,16 @@
 /**
  * Created by minudika on 2/22/16.
  */
+import com.google.common.base.Splitter;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.wso2.siddhi.core.ExecutionPlanRuntime;
+import org.wso2.siddhi.core.SiddhiManager;
+import org.wso2.siddhi.core.event.Event;
+import org.wso2.siddhi.core.stream.input.InputHandler;
+import org.wso2.siddhi.core.stream.output.StreamCallback;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,6 +22,16 @@ import java.util.List;
  */
 public class ObjectEchoClientHandler extends ChannelInboundHandlerAdapter {
 
+    private SiddhiManager siddhiManager = new SiddhiManager();
+    private Logger log = LoggerFactory.getLogger(Executor.class);
+    private static Splitter splitter = Splitter.on(',');
+
+    private static InputHandler inputHandler;
+    private static ExecutionPlanRuntime executionPlanRuntime;
+    String inStreamDefinition;
+    String query;
+
+
     private final List<Integer> firstMessage;
 
     /**
@@ -24,19 +42,72 @@ public class ObjectEchoClientHandler extends ChannelInboundHandlerAdapter {
         for (int i = 0; i < ObjectEchoClient.SIZE; i ++) {
             firstMessage.add(Integer.valueOf(i));
         }
+
+        /*inStreamDefinition = "@config(async = 'true')define stream inputStream (ticker string, date string, time string, askPrice float);\n";
+        query = "from inputStream#window.length(10)\n" +
+                "select ticker, avg(askPrice) as avg_price\n" +
+                "insert into outputStream\n";
+
+        executionPlanRuntime = siddhiManager.createExecutionPlanRuntime(inStreamDefinition + query);
+
+
+        inputHandler = executionPlanRuntime.getInputHandler("inputStream");
+        executionPlanRuntime.start();*/
+        System.err.println("channel registered..");
     }
 
     @Override
     public void channelActive(ChannelHandlerContext ctx) {
         // Send the first message if this handler is a client-side handler.
         //ctx.writeAndFlush(firstMessage);
+       /* inStreamDefinition = "@config(async = 'true')define stream inputStream (ticker string, date string, time string, askPrice float);\n";
+        query = "from inputStream#window.length(10)\n" +
+                "select ticker, avg(askPrice) as avg_price\n" +
+                "insert into outputStream\n";
+
+        executionPlanRuntime = siddhiManager.createExecutionPlanRuntime(inStreamDefinition + query);
+
+
+        inputHandler = executionPlanRuntime.getInputHandler("inputStream");
+        executionPlanRuntime.start();
+        System.err.println("channel registered..");*/
     }
 
     @Override
-    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws InterruptedException {
         // Echo back the received object to the server.
         ctx.write(msg);
         System.err.println("received : "+msg);
+
+        inStreamDefinition = "@config(async = 'true')define stream inputStream (ticker string, date string, time string, askPrice float);\n";
+        query = "from inputStream#window.length(10)\n" +
+                "select ticker, avg(askPrice) as avg_price\n" +
+                "insert into outputStream\n";
+
+        executionPlanRuntime = siddhiManager.createExecutionPlanRuntime(inStreamDefinition + query);
+
+
+        inputHandler = executionPlanRuntime.getInputHandler("inputStream");
+        executionPlanRuntime.start();
+
+        executionPlanRuntime.addCallback("outputStream", new StreamCallback() {
+
+            @Override
+            public void receive(org.wso2.siddhi.core.event.Event[] events) {
+                //EventPrinter.print(events);
+
+                for (org.wso2.siddhi.core.event.Event evt : events) {
+                    Object[] dt = evt.getData();
+                    // System.out.println(dt[0]);//log.info(dt[0]);
+                    //    log.error(dt[0]+" -> "+dt[1]);
+                    System.err.println(dt[0]+" -> "+dt[1]);
+                }
+            }
+        });
+
+        Object eventData = null;
+        inputHandler.send((Event)msg);
+
     }
 
     @Override
